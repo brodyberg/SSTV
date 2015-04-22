e94979e115d1466908564d785d7caffe450f29fa
Brody Berg <brodyberg@gmail.com>
2015-04-21 21:16:59 -0700

    Ref NAudio, create abstract & implementation

---
+++ UseNAudio/UseNAudio/Program.fs
@@ -0,0 +1,21 @@
+﻿// http://mark-dot-net.blogspot.com/2009/10/playback-of-sine-wave-in-naudio.html
+
+open NAudio
+
+[<AbstractClass>]
+type WaveProvider32 () =
+    abstract member SetWaveFormat: int -> int -> unit
+    abstract member Read: (byte array * int * int) -> int
+    abstract member Read: (float array * int * int) -> int
+
+type SineWaveProvider32() =
+    inherit WaveProvider32()
+
+    override this.SetWaveFormat sampleRate channels = ()
+    override this.Read(foo: float array * int * int) = 0
+    override this.Read(foo: byte array * int * int) = 0
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
