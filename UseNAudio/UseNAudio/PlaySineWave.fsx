0cb0e4ea7e94a05b984444d248c0e254ad35e328
Brody Berg <brodyberg@gmail.com>
2015-04-22 20:26:14 -0700

    Working abstract, implementation & use

--- UseNAudio/UseNAudio/PlaySineWave.fsx
+++ UseNAudio/UseNAudio/PlaySineWave.fsx
@@ -8,33 +8,17 @@ open NAudio
 [<AbstractClass>]
 type WaveProvider32 () =
     abstract member SetWaveFormat: int -> int -> unit
-    abstract member Read: byte array -> int -> int -> int
-    abstract member Read: float array -> int -> int -> int
-//    abstract member Read: float array * int * int -> int
-//    abstract member Bar: (int * int * int) -> int
+    abstract member Read: (byte array * int * int) -> int
+    abstract member Read: (float array * int * int) -> int
 
 type SineWaveProvider32() =
     inherit WaveProvider32()
 
     override this.SetWaveFormat sampleRate channels = ()
-    override this.Read floats these that = 0
-    override this.Read bytes these that = -1
-//    override this.Read' top left bottom = -1
-//    override this.Read bytes these that = 0
-//    override this.Bar(bar:int*int*int) = 1
+    override this.Read (x:(byte array * int * int)) = -3
+    override this.Read (y:(float array * int * int)) = -4
 
-let x = SineWaveProvider32()
+let n = ([| 86uy |], 0, 0)
 
-x.SetWaveFormat 0 0
-x.Read()
-
-x.Read([||], 0, 0)
-
-x.Read()
-2 + 2
-
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
-
+let y = SineWaveProvider32()
+y.Read n
