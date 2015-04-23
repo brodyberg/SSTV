a33888599a129555f65138c856f6e8163f374ccc
Brody Berg <brodyberg@gmail.com>
2015-04-22 21:26:48 -0700

    Try object expression instead

--- UseNAudio/UseNAudio/PlaySineWave.fsx
+++ UseNAudio/UseNAudio/PlaySineWave.fsx
@@ -4,15 +4,31 @@
 #r "NAudio.dll"
 
 open NAudio
+open NAudio.Wave
+
+let makeSound (sampleRate:int) (channels:int) = 
+    { new NAudio.Wave.IWaveProvider
+      with 
+          member this.Read(buffer,offset,count) = -1 
+          member this.WaveFormat with get() = WaveFormat.CreateIeeeFloatWaveFormat(sampleRate,channels) }
+
+let x = makeSound 0 0 
+
+x.WaveFormat
 
 [<AbstractClass>]
 type WaveProvider32 () =
+//    new: int -> int -> WaveProvider32
+//    inherit IWaveProvider
+
     abstract member SetWaveFormat: int -> int -> unit
     abstract member Read: (byte array * int * int) -> int
     abstract member Read: (float array * int * int) -> int
 
 type SineWaveProvider32() =
     inherit WaveProvider32()
+
+    new 
 
     override this.SetWaveFormat sampleRate channels = ()
     override this.Read (x:(byte array * int * int)) = -3
