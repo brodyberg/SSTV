9ad759377b17cc57825eda17ba9419e6ec1081f3
Brody Berg <brodyberg@gmail.com>
2015-04-23 17:02:16 -0700

    Babysteps

--- UseNAudio/UseNAudio/PlaySineWave.fsx
+++ UseNAudio/UseNAudio/PlaySineWave.fsx
@@ -6,35 +6,21 @@
 open NAudio
 open NAudio.Wave
 
+let readFloat (x:(float32 array * int)) = -1
+
 let makeSound (sampleRate:int) (channels:int) = 
     { new NAudio.Wave.IWaveProvider
+
       with 
-          member this.Read(buffer,offset,count) = -1 
+          member this.Read(buffer,offset,count) = 
+            let wb = WaveBuffer(buffer)
+            let samplesRequired = count / 4
+            let samplesRead = readFloat(wb.FloatBuffer, offset / samplesRequired)
+    
+            samplesRead * 4
+             
           member this.WaveFormat with get() = WaveFormat.CreateIeeeFloatWaveFormat(sampleRate,channels) }
 
-let x = makeSound 0 0 
+let x = makeSound 44100 1 
 
 x.WaveFormat
-
-[<AbstractClass>]
-type WaveProvider32 () =
-//    new: int -> int -> WaveProvider32
-//    inherit IWaveProvider
-
-    abstract member SetWaveFormat: int -> int -> unit
-    abstract member Read: (byte array * int * int) -> int
-    abstract member Read: (float array * int * int) -> int
-
-type SineWaveProvider32() =
-    inherit WaveProvider32()
-
-    new 
-
-    override this.SetWaveFormat sampleRate channels = ()
-    override this.Read (x:(byte array * int * int)) = -3
-    override this.Read (y:(float array * int * int)) = -4
-
-let n = ([| 86uy |], 0, 0)
-
-let y = SineWaveProvider32()
-y.Read n
