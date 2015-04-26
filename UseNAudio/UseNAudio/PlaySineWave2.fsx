6d9d4549f2fd6e5fe027b7f51b23cc4f01529dd5
Brody Berg <brodyberg@gmail.com>
2015-04-26 12:54:02 -0700

    Restart

---
+++ UseNAudio/UseNAudio/PlaySineWave2.fsx
@@ -0,0 +1,34 @@
+﻿
+// float Frequency
+// float Amplitude
+
+// empty ctor
+// derives WaveProvider32
+
+// overrides Read(float array * int * int) -> int
+
+#I @"..\packages\NAudio.1.7.3\lib\net35\"
+#r "NAudio.dll"
+
+open NAudio
+open NAudio.Wave
+
+type SineWaveProvider() =
+    inherit NAudio.Wave.WaveProvider32()
+
+    let frequency = 1000.0
+    let amplitude = 0.25f
+
+    member this.Frequency = frequency
+    member this.Amplitude = amplitude
+
+    override this.Read() = -1
+
+
+
+
+
+
+
+
+
