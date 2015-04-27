2d783bf30f7fc5dbb136290dc397f4a51f136e2a
Brody Berg <brodyberg@gmail.com>
2015-04-26 21:29:49 -0700

    Should work, but I can't hear anything

--- UseNAudio/UseNAudio/PlaySineWave2.fsx
+++ UseNAudio/UseNAudio/PlaySineWave2.fsx
@@ -5,7 +5,7 @@ open System
 open NAudio
 open NAudio.Wave
 
-type SineWaveProvider() =
+type SineWaveProvider32() =
     inherit NAudio.Wave.WaveProvider32()
 
     let frequency = 1000.0
@@ -25,6 +25,7 @@ type SineWaveProvider() =
     override this.Read(buffer:float32 [], offset:int, sampleCount:int) =
         let sampleRate = base.WaveFormat.SampleRate
 
+        // Fold across simple sequence, so there's no mutation of sample
         seq { for i in 0 .. sampleCount -> 
                 iterateAndCapSample sampleRate
                 sin32 (2.0 * Math.PI * sample * this.Frequency)
@@ -33,8 +34,57 @@ type SineWaveProvider() =
 
         sampleCount
 
+let sineWaveProvider = SineWaveProvider32()
+sineWaveProvider.SetWaveFormat(16000, 1) // 16khz mono
+let waveOut = new WaveOut()
+waveOut.Init(sineWaveProvider)
+
+waveOut.Play()
+
+waveOut.Stop()
+// sineWaveProvider.Frequency 
+//sineWaveProvider.Frequency <- 1000
 
 
 
 
-
+//type SineWaveProvider'() =
+//    inherit NAudio.Wave.WaveProvider32()
+//
+//    let frequency = 1000.0
+//    let amplitude = 0.25
+//
+//    let sin32 a = (float32) (System.Math.Sin a)
+//
+//    member this.Frequency = frequency
+//    member this.Amplitude = amplitude
+//    override this.Read(buffer:float32 [], offset:int, sampleCount:int) =
+//        let sampleRate = base.WaveFormat.SampleRate
+//
+//        // Fold across simple sequence, so there's no mutation of sample
+//        seq { for i in 0 .. sampleCount -> i }
+//        |> Seq.fold (fun (sample, item) -> 
+//                let sample' = sample + 1.0
+//
+//                // match
+//                if sample' >= (float) sampleRate
+//                then sample <- 0.0)
+//
+//                sin32 (2.0 * Math.PI * sample * this.Frequency)
+//
+//
+//        seq { for i in 0 .. sampleCount -> 
+//                iterateAndCapSample sampleRate
+//                sin32 (2.0 * Math.PI * sample * this.Frequency)
+//            }
+//        |> Seq.iteri (fun i item -> buffer.[i+offset] <- (float32) item)
+//
+//        sampleCount
+//
+//
+//
+//
+//
+//
+//
+//
